878a75c1a15102fe8ca626dcae6074fe52e4f362
Peter Swanson <topazoo42@gmail.com>
2023-03-07 23:25:22 -0800

    [Client] Add base client

--- client/main.py
+++ client/main.py
@@ -1,13 +1,14 @@
 import os, openai
 
 class OpenAPI_Client():
-    ''' Client to interact with the OpenAI API '''
+    ''' Base client to interact with the OpenAI API '''
 
     _api_key = ""
     _temperature = 0
     _model = "text-davinci-003"
+    _api = openai.Completion
 
-    def __init__(self, api_key:str=None, defaul_model_name:str="text-davinci-003", default_temperature=0) -> None:
+    def __init__(self, api_key:str=None, defaul_model_name:str="", default_temperature=0) -> None:
 
         # Read passed API key but default to reading from environmental variables
         openai.api_key = self._api_key = api_key or os.environ.get('OPENAI_API_KEY')
@@ -16,16 +17,15 @@ class OpenAPI_Client():
 
         # TODO - Models enum check
         # Read passed default model but default to reading from environmental variables
-        self._model = defaul_model_name or os.environ.get('OPENAI_DEFAULT_MODEL')
+        self._model = defaul_model_name or os.environ.get('OPENAI_DEFAULT_MODEL', self._model)
 
         # Read passed default temperature but default to reading from environmental variables
-        self._temperature = default_temperature or os.environ.get('OPENAI_DEFAULT_TEMPERATURE')
+        self._temperature = default_temperature or os.environ.get('OPENAI_DEFAULT_TEMPERATURE', self._temperature)
 
 
     def run_prompt(self, prompt:str, model:str=None, temperature:float=None):
-        
         # Allow the model and temperature to be specified per call, but fall back on defaults
         model = model or self._model
         temperature = temperature or self._temperature
 
-        return openai.Completion.create(prompt=prompt, model=model, temperature=temperature)
+        return self._api.create(prompt=prompt, model=model, temperature=temperature)
