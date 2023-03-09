d304ba31f7bf011b23181e5bf554bc15c0a51ec5
Peter Swanson <topazoo42@gmail.com>
2023-03-08 17:07:11 -0800

    [Structure] Add standard Python library structure

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# Clients
+from .clients import Chat_Bot_Client
+
+# Base classes / mixins
+from .base import OpenAPI_Client
+from .mixins import Chat_Context_Manager_Mixin
+
+# Enums
+from .enums import ROLE
